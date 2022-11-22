1d31fa6bdbce88d8cd0516f31eca791155969bf6
Sachin Vishwakrma <59968469+ig-sachin@users.noreply.github.com>
2022-11-22 15:38:48 +0530

    sc

--- index.sol
+++ index.sol
@@ -12,11 +12,27 @@ contract crowdfunding {
     uint public raisedAmount;
     uint public noOfContributors;
 
+    struct Request {
+        string eventName;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address => bool) voters;
+    }
+    mapping(uint => Request) public requests;
+    uint public numRequests;
+
     constructor(uint _target, uint _deadline) {
         target = _target;
         deadline = block.timestamp + _deadline;
         minimumContribution = 100 wei;
         manager = msg.sender;
+    }
+
+    modifier onlyManager() {
+        require(manager != msg.sender, "Manager Cannot contribute here");
+        _;
     }
 
     function sendEth() public payable {
@@ -47,5 +63,45 @@ contract crowdfunding {
         address payable user = payable(msg.sender);
         user.transfer(contributors[msg.sender]);
         contributors[msg.sender] = 0;
+    }
+
+    function createRequest(
+        string memory _description,
+        address payable _recipient,
+        uint _value
+    ) public onlyManager {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.eventName = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+
+    function voteRequest(uint _requestNo) public {
+        require(contributors[msg.sender] > 0, "You must be contributor");
+        Request storage thisRequest = requests[_requestNo];
+        require(
+            thisRequest.voters[msg.sender] == false,
+            "You have already voted"
+        );
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint _requestNo) public onlyManager {
+        require(raisedAmount >= target);
+        Request storage thisRequest = requests[_requestNo];
+        require(
+            thisRequest.completed == false,
+            "The request has been completed"
+        );
+        require(
+            thisRequest.noOfVoters > noOfContributors / 2,
+            "Majority does not support"
+        );
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
     }
 }
